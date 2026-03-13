1a0c2ad57c2a1494c15369af9937dd4fd0d4c694
G'iyosiddin Ubaydullayev <ubaydullayevgiyosiddin7@gmail.com>
2026-03-13 14:02:32 +0500

    Add Leaflet map to xarita tab and fix media serving in production

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/tma/', permanent=False)),
     path('', include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
